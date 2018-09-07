4f8a2ca17f658d0ad3efdde22be71b640a322d0d
Gabriela Suriano <surianogabriela@gmail.com>
2018-09-07 10:49:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
     return array[0]
   elsif array.size == 2 
     return array.join(" and ")
+  elsif array.size == 3 
+    return array.join(" , ") + (" ,and ")
   else 
     return array[0..-2].join(" , ") + (" ,and ") + array[-1]
   end
